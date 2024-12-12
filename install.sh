0679e75ce2f041b1e5f06b9352bf89905dcb07fe
itka0526 <itka0526@gmail.com>
2024-12-12 05:01:39 +0300

    normal

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-sudo -s
 wget https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.1.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
